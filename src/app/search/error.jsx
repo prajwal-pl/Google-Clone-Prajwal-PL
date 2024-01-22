ab41a599aa4255998be5448f265dc5edd231288e
Prajwal-PL <prajwalpl096@gmail.com>
2024-01-22 22:10:11 +0530

    fixed error

--- src/app/search/error.jsx
+++ src/app/search/error.jsx
@@ -2,10 +2,10 @@
 import { useEffect } from 'react'
 import React from 'react'
 
-export default function error(error, reset) {
-    useEffect(()=>{
-        console.log("Error: ", error)
-    }, {error})
+export default function error(reset) {
+    // useEffect(()=>{
+    //     console.log("Error: ", error)
+    // }, {error})
   return (
     <div className='flex flex-col justify-center items-center pt-10 '>
         <h1 className='text-3xl mb-4'>Something Went Wrong</h1>
